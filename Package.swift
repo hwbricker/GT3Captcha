0d6611bf7b4eb05af1bb94157e2979e8adebbfb3
wei.cao <wei.cao@xjmz.com>
2024-06-26 20:56:53 +0800

    add gt3captcha

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "GT3Captcha",
+    defaultLocalization: "zh_CN",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(name: "GT3Captcha", targets: ["GT3Captcha"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "GT3Captcha", path: "./Sources/GT3Captcha.xcframework")
+    ]
+)
